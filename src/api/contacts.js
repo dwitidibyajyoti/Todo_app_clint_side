4a2404e9c945f586d9c709adb06287744f179de2
dwitidibyajyoti sahoo <dwitidibyajyoti@gmail.com>
2021-03-06 04:05:45 +0530

    change for production

--- src/api/contacts.js
+++ src/api/contacts.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:8000/',
+  baseURL: 'https://todo-app-dwiti.herokuapp.com/',
 });
+// baseURL: 'http://localhost:8000/'
